2ae942648c338809a50aaf14e3239994160e5d6d
MatthieuAQB <mattaqb@gmail.com>
2026-05-10 15:57:48 +0200

    Ajout commentaire dans Sequelize

--- api/config/database.js
+++ api/config/database.js
@@ -3,15 +3,18 @@ const dotenv = require("dotenv");
 // dotenv s'active avec .config juste après l'import
 dotenv.config();
 
+// Création d'une instance Sequelize pour se connecter à la base de données MySQL
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
   process.env.DB_PASSWORD,
   {
+    // Configuration de la connexion à la base de données
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     dialect: "mysql",
   },
 );
 
-mdodule.exports = sequelize;
+// exportation de l'instance Sequelize pour l'utiliser dans d'autres parties de l'application
+module.exports = sequelize;
