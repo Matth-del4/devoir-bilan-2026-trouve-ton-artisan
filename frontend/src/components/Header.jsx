ec3fd91cee9f1883b996cf3c766db1a7df368e69
MatthieuAQB <mattaqb@gmail.com>
2026-06-20 11:28:32 +0200

    FEAT : Création d'un component header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,13 +1,56 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import { useEffect } from "react";
+import axios from "axios";
 
 function Header() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [categories, setCategories] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/api/categories", {
+        headers: {
+          "x-api-key": import.meta.env.VITE_API_KEY,
+        },
+      })
+      .then((response) => {
+        setCategories(response.data);
+      });
+  }, []);
+
   return (
     <header>
-      <h1>Trouve ton artisan</h1>
+      <button
+        className="menu-button"
+        onClick={() => setIsMenuOpen(!isMenuOpen)}
+      >
+        Menu
+      </button>
+
+      <input
+        type="text"
+        placeholder="Rechercher..."
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+
       <nav>
-        <Link to="/">logo</Link>
-        <Link to="/about">À propos</Link>
+        <Link to="/">
+          <h1>Trouve ton artisan</h1>
+        </Link>
       </nav>
+
+      {isMenuOpen && (
+        <nav>
+          {categories.map((category) => (
+            <Link key={category.id} to={`/category/${category.id}`}>
+              {category.domaine}
+            </Link>
+          ))}
+        </nav>
+      )}
     </header>
   );
 }
